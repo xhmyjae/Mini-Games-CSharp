811d7b5a8d0b3532f76f069cd22b94825cbe040a
xhmyjae <tomatis.mt@gmail.com>
2022-10-11 12:30:27 +0200

    feat : finish pong code (needs design elements)

--- MiniGames/Pong.Designer.cs
+++ MiniGames/Pong.Designer.cs
@@ -35,12 +35,16 @@ namespace MiniGames
             this.scorePlayer1 = new System.Windows.Forms.Label();
             this.scorePlayer2 = new System.Windows.Forms.Label();
             this.boxPlayer1 = new System.Windows.Forms.PictureBox();
-            this.boxPLayer2 = new System.Windows.Forms.PictureBox();
+            this.boxPlayer2 = new System.Windows.Forms.PictureBox();
             this.ball = new System.Windows.Forms.PictureBox();
+            this.timeRepeat = new System.Timers.Timer();
+            this.winTextPlayer1 = new System.Windows.Forms.Label();
+            this.winTextPlayer2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.midLine)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.boxPlayer1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.boxPLayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxPlayer2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeRepeat)).BeginInit();
             this.SuspendLayout();
             // 
             // midLine
@@ -81,14 +85,14 @@ namespace MiniGames
             this.boxPlayer1.TabIndex = 3;
             this.boxPlayer1.TabStop = false;
             // 
-            // boxPLayer2
+            // boxPlayer2
             // 
-            this.boxPLayer2.BackColor = System.Drawing.Color.Chartreuse;
-            this.boxPLayer2.Location = new System.Drawing.Point(784, 217);
-            this.boxPLayer2.Name = "boxPLayer2";
-            this.boxPLayer2.Size = new System.Drawing.Size(36, 162);
-            this.boxPLayer2.TabIndex = 4;
-            this.boxPLayer2.TabStop = false;
+            this.boxPlayer2.BackColor = System.Drawing.Color.Chartreuse;
+            this.boxPlayer2.Location = new System.Drawing.Point(784, 217);
+            this.boxPlayer2.Name = "boxPlayer2";
+            this.boxPlayer2.Size = new System.Drawing.Size(36, 162);
+            this.boxPlayer2.TabIndex = 4;
+            this.boxPlayer2.TabStop = false;
             // 
             // ball
             // 
@@ -99,13 +103,44 @@ namespace MiniGames
             this.ball.TabIndex = 5;
             this.ball.TabStop = false;
             // 
+            // timeRepeat
+            // 
+            this.timeRepeat.Enabled = true;
+            this.timeRepeat.Interval = 10D;
+            this.timeRepeat.SynchronizingObject = this;
+            this.timeRepeat.Elapsed += new System.Timers.ElapsedEventHandler(this.timeRepeat_Elapsed);
+            // 
+            // winTextPlayer1
+            // 
+            this.winTextPlayer1.Font = new System.Drawing.Font("Verdana", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winTextPlayer1.ForeColor = System.Drawing.Color.Ivory;
+            this.winTextPlayer1.Location = new System.Drawing.Point(270, 98);
+            this.winTextPlayer1.Name = "winTextPlayer1";
+            this.winTextPlayer1.Size = new System.Drawing.Size(287, 49);
+            this.winTextPlayer1.TabIndex = 7;
+            this.winTextPlayer1.Text = "Player 1 WON !";
+            this.winTextPlayer1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winTextPlayer2
+            // 
+            this.winTextPlayer2.Font = new System.Drawing.Font("Verdana", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winTextPlayer2.ForeColor = System.Drawing.Color.Ivory;
+            this.winTextPlayer2.Location = new System.Drawing.Point(263, 98);
+            this.winTextPlayer2.Name = "winTextPlayer2";
+            this.winTextPlayer2.Size = new System.Drawing.Size(294, 49);
+            this.winTextPlayer2.TabIndex = 8;
+            this.winTextPlayer2.Text = "Player 2 WON !";
+            this.winTextPlayer2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Pong
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
             this.ClientSize = new System.Drawing.Size(832, 591);
+            this.Controls.Add(this.winTextPlayer2);
+            this.Controls.Add(this.winTextPlayer1);
             this.Controls.Add(this.ball);
-            this.Controls.Add(this.boxPLayer2);
+            this.Controls.Add(this.boxPlayer2);
             this.Controls.Add(this.boxPlayer1);
             this.Controls.Add(this.scorePlayer2);
             this.Controls.Add(this.scorePlayer1);
@@ -116,15 +151,23 @@ namespace MiniGames
             this.Text = "Pong";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Pong_FormClosing);
             this.Load += new System.EventHandler(this.Pong_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Pong_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Pong_KeyUp);
             this.Resize += new System.EventHandler(this.Pong_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.midLine)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.boxPlayer1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.boxPLayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxPlayer2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeRepeat)).EndInit();
             this.ResumeLayout(false);
         }
 
-        private System.Windows.Forms.PictureBox boxPLayer2;
+        private System.Windows.Forms.Label winTextPlayer1;
+        private System.Windows.Forms.Label winTextPlayer2;
+
+        private System.Timers.Timer timeRepeat;
+
+        private System.Windows.Forms.PictureBox boxPlayer2;
 
         private System.Windows.Forms.PictureBox boxPlayer1;
 
--- MiniGames/Pong.cs
+++ MiniGames/Pong.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using System.Drawing;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace MiniGames
 {
     public partial class Pong : Form
     {
+        // Resize
         private Rectangle MidLineRectangle;
         private Rectangle BoxPlayer1Rectangle;
         private Rectangle BoxPlayer2Rectangle;
         private Rectangle BallRectangle;
         private Rectangle ScorePlayer1Rectangle;
         private Rectangle ScorePlayer2Rectangle;
+        private Rectangle WinTextPlayer1Rectangle;
+        private Rectangle WinTextPlayer2Rectangle;
         private Size OriginalFormSize;
+        
+        // Gameplay
+        private bool Player1GoUp;
+        private bool Player1GoDown;
+        private bool PLayer2GoUp;
+        private bool Player2GoDown;
+        private int BallDirectionY = 7;
+        private int BallDirectionX = 7;
+
+        private int ScorePlayer1 = 0;
+        private int ScorePlayer2 = 0;
 
         public Pong()
         {
@@ -24,22 +39,24 @@ namespace MiniGames
             OriginalFormSize = this.Size;
             MidLineRectangle = new Rectangle(midLine.Location.X, midLine.Location.Y, midLine.Width, midLine.Height);
             BoxPlayer1Rectangle = new Rectangle(boxPlayer1.Location.X, boxPlayer1.Location.Y, boxPlayer1.Width, boxPlayer1.Height);
-            BoxPlayer2Rectangle = new Rectangle(boxPLayer2.Location.X, boxPLayer2.Location.Y, boxPLayer2.Width, boxPLayer2.Height);
+            BoxPlayer2Rectangle = new Rectangle(boxPlayer2.Location.X, boxPlayer2.Location.Y, boxPlayer2.Width, boxPlayer2.Height);
             BallRectangle = new Rectangle(ball.Location.X, ball.Location.Y, ball.Width, ball.Height);
             ScorePlayer1Rectangle = new Rectangle(scorePlayer1.Location.X, scorePlayer1.Location.Y, scorePlayer1.Width, scorePlayer1.Height);
             ScorePlayer2Rectangle = new Rectangle(scorePlayer2.Location.X, scorePlayer2.Location.Y, scorePlayer2.Width, scorePlayer2.Height);
-            
+            WinTextPlayer1Rectangle = new Rectangle(winTextPlayer1.Location.X, winTextPlayer1.Location.Y, winTextPlayer1.Width, winTextPlayer1.Height);
+            WinTextPlayer2Rectangle = new Rectangle(winTextPlayer2.Location.X, winTextPlayer2.Location.Y, winTextPlayer2.Width, winTextPlayer2.Height);
+
             // Places ball at the center of the screen
             ball.Left = (this.ClientSize.Width - ball.Width) / 2;
             ball.Top = (this.ClientSize.Height - ball.Height) / 2;
-            midLine.Left = (this.ClientSize.Height - midLine.Height) / 2;
+            midLine.Left = (this.ClientSize.Width - ball.Width) / 2;
         }
         
         private void Pong_Resize(object sender, EventArgs e)
         {
             Utils.ResizeButtons(MidLineRectangle, midLine, this, OriginalFormSize);
             Utils.ResizeButtons(BoxPlayer1Rectangle, boxPlayer1, this, OriginalFormSize);
-            Utils.ResizeButtons(BoxPlayer2Rectangle, boxPLayer2, this, OriginalFormSize);
+            Utils.ResizeButtons(BoxPlayer2Rectangle, boxPlayer2, this, OriginalFormSize);
             Utils.ResizeButtons(BallRectangle, ball, this, OriginalFormSize);
             Utils.ResizeButtons(ScorePlayer1Rectangle, scorePlayer1, this, OriginalFormSize);
             Utils.ResizeButtons(ScorePlayer2Rectangle, scorePlayer2, this, OriginalFormSize);
@@ -49,6 +66,117 @@ namespace MiniGames
         {
             Form1 menu = new Form1();
             menu.Show();
+        }
+
+        private void Pong_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W :
+                    Player1GoUp = true;
+                    break;
+                case Keys.S :
+                    Player1GoDown = true;
+                    break;
+                case Keys.Up :
+                    PLayer2GoUp = true;
+                    break;
+                case Keys.Down :
+                    Player2GoDown = true;
+                    break;
+            }
+        }
+
+        private void Pong_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W :
+                    Player1GoUp = false;
+                    break;
+                case Keys.S :
+                    Player1GoDown = false;
+                    break;
+                case Keys.Up :
+                    PLayer2GoUp = false;
+                    break;
+                case Keys.Down :
+                    Player2GoDown = false;
+                    break;
+            }
+        }
+
+        private void timeRepeat_Elapsed(object sender, ElapsedEventArgs e)
+        {
+             // Set scores
+            scorePlayer1.Text = "" + ScorePlayer1;
+            scorePlayer2.Text = "" + ScorePlayer2;
+
+            winTextPlayer1.Visible = false;
+            winTextPlayer2.Visible = false;
+
+            ball.Top -= BallDirectionY;
+            ball.Left -= BallDirectionX;
+
+            // Check lose
+            if (ball.Left < 0)
+            {
+                ball.Left = (this.ClientSize.Width - ball.Width) / 2;
+                ball.Top = (this.ClientSize.Height - ball.Height) / 2;
+                ScorePlayer2++;
+            }
+
+            if (ball.Left + ball.Width > ClientSize.Width)
+            {
+                ball.Left = (this.ClientSize.Width - ball.Width) / 2;
+                ball.Top = (this.ClientSize.Height - ball.Height) / 2;
+                ScorePlayer1++;
+            }
+            
+            // Make ball rebound
+            if (ball.Top < 0 || ball.Top + ball.Height > ClientSize.Height)
+            {
+                BallDirectionY = -BallDirectionY;
+            }
+            
+            if (ball.Bounds.IntersectsWith(boxPlayer1.Bounds) || ball.Bounds.IntersectsWith(boxPlayer2.Bounds))
+            {
+                BallDirectionX = -BallDirectionX;
+            }
+            
+            // Control the players box
+            if (Player1GoDown && boxPlayer1.Bottom < ClientSize.Height)
+            {
+                boxPlayer1.Top += 7;
+            }
+            
+            if (Player1GoUp && boxPlayer1.Top > 0)
+            {
+                boxPlayer1.Top -= 7;
+            }
+            
+            if (PLayer2GoUp && boxPlayer2.Top > 0)
+            {
+                boxPlayer2.Top -= 7;
+            }
+            
+            if (Player2GoDown && boxPlayer2.Bottom < ClientSize.Height)
+            {
+                boxPlayer2.Top += 7;
+            }
+            
+            // Check scores
+            if (ScorePlayer1 > 10)
+            {
+                timeRepeat.Stop();
+                winTextPlayer1.Visible = true;
+            }
+
+            if (ScorePlayer2 > 10)
+            {
+                timeRepeat.Stop();
+                winTextPlayer2.Visible = true;
+            }
         }
     }
 }
