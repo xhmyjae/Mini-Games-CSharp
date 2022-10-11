15bf38ff135c99107cb6362438bce5d445049f5f
xhmyjae <tomatis.mt@gmail.com>
2022-10-11 23:11:10 +0200

    feat : add color to platformer background + add first block

--- MiniGames/Platformer.Designer.cs
+++ MiniGames/Platformer.Designer.cs
@@ -31,11 +31,34 @@ namespace MiniGames
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bloc = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize) (this.bloc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bloc
+            // 
+            this.bloc.BackColor = System.Drawing.Color.Ivory;
+            this.bloc.Location = new System.Drawing.Point(-1, 532);
+            this.bloc.Name = "bloc";
+            this.bloc.Size = new System.Drawing.Size(60, 60);
+            this.bloc.TabIndex = 0;
+            this.bloc.TabStop = false;
+            // 
+            // Platformer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (24)))), ((int) (((byte) (24)))));
+            this.ClientSize = new System.Drawing.Size(832, 591);
+            this.Controls.Add(this.bloc);
+            this.Name = "Platformer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Platformer";
+            ((System.ComponentModel.ISupportInitialize) (this.bloc)).EndInit();
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.PictureBox bloc;
 
         #endregion
     }
