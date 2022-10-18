41264fa65d9489f5a88fe6de4431b7533c9c2879
xhmyjae <tomatis.mt@gmail.com>
2022-10-18 11:16:59 +0200

    feat : collisions player/platforms working

--- MiniGames/PlatRat.cs
+++ MiniGames/PlatRat.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.Linq;
 using System.Threading;
 using System.Timers;
 using System.Windows.Forms;
@@ -9,13 +10,16 @@ namespace MiniGames
 {
     public partial class PlatRat : Form
     {
-        private bool Player1GoLeft, Player1GoRight, Player1Jump, TouchedEnemies, Player1OnGround;
-        private int PlayerSpeed = 5;
+        private bool Player1IsLeft, Player1IsRight, Player1IsJumping, IsTouchingEnemies, Player1IsOnGround;
+        private int Player1Speed = 5;
+        private int Player1JumpSpeed = 15;
+        private int Force;
+        private int Player1Lives = 3;
+
         private int Enemy1Speed = 3;
         private int Enemy2Speed = 4;
-        private int PlayerJumpSpeed = 100;
-        private int Player1Lives = 3;
         private int EnemiesTimer = 0;
+        
         private int Score;
 
         public PlatRat()
@@ -34,17 +38,18 @@ namespace MiniGames
             switch (e.KeyCode)
             {
                 case Keys.Right :
-                    Player1GoRight = true;
+                    Player1IsRight = true;
                     player1.Image = Properties.Resources.cropRat;
                     break;
                 case Keys.Left :
-                    Player1GoLeft = true;
+                    Player1IsLeft = true;
                     player1.Image = Properties.Resources.reverseCropRat;
                     break;
                 case Keys.Up :
-                    if (!Player1Jump)
+                    if (!Player1IsJumping)
                     {
-                        Player1Jump = true;
+                        Player1IsJumping = true;
+                        Force = Player1JumpSpeed;
                     }
                     break;
             }
@@ -55,10 +60,10 @@ namespace MiniGames
             switch (e.KeyCode)
             {
                 case Keys.Right :
-                    Player1GoRight = false;
+                    Player1IsRight = false;
                     break;
                 case Keys.Left :
-                    Player1GoLeft = false;
+                    Player1IsLeft = false;
                     break;
                 // case Keys.Up :
                 //     Player1Jump = false;
@@ -72,77 +77,104 @@ namespace MiniGames
             enemy1.BringToFront();
             enemy2.BringToFront();
             message.Text = "";
-            if (TouchedEnemies)
+            
+            // Set scores and cheeses Count
+            livesCount.Text = Player1Lives.ToString();
+            cheesesCount.Text = Score.ToString();
+            
+            // Checks if player touched enemies
+            if (IsTouchingEnemies)
             {
                 EnemiesTimer += 10;
                 if (EnemiesTimer >= 700)
                 {
                     enableEnemies(enemy1);
                     enableEnemies(enemy2);
-                    TouchedEnemies = false;
+                    IsTouchingEnemies = false;
                 }
             }
 
-            // Set scores and cheeses Count
-            livesCount.Text = Player1Lives.ToString();
-            cheesesCount.Text = Score.ToString();
-            
             // Make characters move
-            player1.Top += PlayerSpeed;
+            player1.Top += Player1Speed;
             
-            if (Player1GoLeft && player1.Left > 0)
+            if (Player1IsLeft && player1.Left > 0)
             {
-                player1.Left -= PlayerSpeed;
+                player1.Left -= Player1Speed;
             }
 
-            if (Player1GoRight && player1.Right < ClientSize.Width)
+            if (Player1IsRight && player1.Right < ClientSize.Width)
             {
-                player1.Left += PlayerSpeed;
+                player1.Left += Player1Speed;
             }
 
-            if (Player1Jump && Player1OnGround)
+            if (Player1IsJumping)
             {
-                player1.Top -= PlayerJumpSpeed;
-                Player1OnGround = false;
+                player1.Top -= Force;
+                Force--;
             }
-
+            
             foreach (Control control in this.Controls)
             {
-
-                if (control.Tag == "bloc")
+                if (control.Tag == "bloc" && player1.Bounds.IntersectsWith(control.Bounds))
                 {
-                    if (player1.Bounds.IntersectsWith(control.Bounds) && !Player1Jump && player1.Bottom < control.Bottom && player1.Right != control.Left && player1.Left != control.Right)
+                    int[] values =
                     {
-                        player1.Top = control.Top - player1.Height;
-                    }
-                    
-                    if (player1.Bounds.IntersectsWith(control.Bounds) && player1.Top > control.Top && player1.Right != control.Left && player1.Left != control.Right)
-                    {
-                        player1.Top = control.Bottom;
-                    }
+                        player1.Bottom - control.Top,
+                        player1.Right - control.Left,
+                        control.Bottom - player1.Top,
+                        control.Right - player1.Left
+                    };
 
-                    if (player1.Bounds.IntersectsWith(control.Bounds) && player1.Right < control.Right && player1.Left < control.Left)
+                    int index = values.Min();
+                    
+                    switch (Array.IndexOf(values, index))
                     {
-                        player1.Left = control.Left - player1.Width;
+                        case 0 :
+                            player1.Top = control.Top - player1.Height;
+                            Force = 0;
+                            Player1IsJumping = false;
+                            break;
+                        case 1 :
+                            player1.Left = control.Left - player1.Width;
+                            break;
+                        case 2 :
+                            player1.Top = control.Bottom;
+                            break;
+                        case 3 :
+                            player1.Left = control.Right;
+                            break;
                     }
                     
-                    if (player1.Bounds.IntersectsWith(control.Bounds) && player1.Left > control.Left && player1.Right > control.Right)
-                    {
-                        player1.Left = control.Right;
-                    }
-                    
-                    // Makes player jump
-                    if (player1.Bottom == control.Top)
-                    {
-                        Player1OnGround = true;
-                        Player1Jump = false;
-                    }
-
+                    // Touching control Top side
+                    // if (player1.Bottom >= control.Top &&  player1.Top < control.Top && player1.Left > control.Left - player1.Width && player1.Right < control.Right + player1.Width)
+                    // {
+                    //     player1.Top = control.Top - player1.Height;
+                    //     Force = 0;
+                    //     Player1IsJumping = false;
+                    // }
+                    //
+                    // // Touching control Bottom side
+                    // if (player1.Top <= control.Bottom && player1.Bottom > control.Bottom && player1.Left > control.Left - player1.Width && player1.Right < control.Right + player1.Width)
+                    // {
+                    //     player1.Top = control.Bottom;
+                    // }
+                    //
+                    // // Touching control Right side
+                    // if (player1.Left <= control.Right && player1.Right > control.Right && player1.Top > control.Top - player1.Height && player1.Bottom < control.Bottom + player1.Height)
+                    // {
+                    //     player1.Left = control.Right;
+                    // }
+                    //
+                    // // Touching control Left side
+                    // if (player1.Right >= control.Left && player1.Left < control.Left && player1.Top > control.Top - player1.Height && player1.Bottom < control.Bottom + player1.Height)
+                    // {
+                    //     player1.Left = control.Left - player1.Width;
+                    // }
                 }
             }
             
             // Make enemies move
-            if (!TouchedEnemies)
+            if (!IsTouchingEnemies)
             {
                 enemy1.Left += Enemy1Speed;
                 if (enemy1.Right >= blocSimple6.Right || enemy1.Left <= blocSimple5.Left)
@@ -162,10 +194,10 @@ namespace MiniGames
             {
                 if (control.Tag == "enemy")
                 {
-                    if (player1.Bounds.IntersectsWith(control.Bounds) && !TouchedEnemies)
+                    if (player1.Bounds.IntersectsWith(control.Bounds) && !IsTouchingEnemies)
                     {
                         EnemiesTimer = 0;
-                        TouchedEnemies = true;
+                        IsTouchingEnemies = true;
                         disableEnemies(enemy1);
                         disableEnemies(enemy2);
                         Player1Lives--;
